f57f53bab4a1d1e89b3439bea35dcb27e8ef23d1
Chandler Tu <chandlertu@chandlertu.com>
2018-01-03 09:48:48 +0800

    Google Style

--- src/main/java/com/chandlertu/checkstyle/test/CheckstyleTestApplication.java
+++ src/main/java/com/chandlertu/checkstyle/test/CheckstyleTestApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CheckstyleTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CheckstyleTestApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(CheckstyleTestApplication.class, args);
+  }
+
 }
